ead1e376396d85fc1d57ed5586895ed0d31a6067
Maxim <m.i.tsenkov@dundee.ac.uk>
2016-01-30 14:52:51 +0000

    Cleaned up cir_realign.py coding and aligned output on Jalview to give unedited_aligned_sequences.fasta file

--- sequences/cir_realign.py
+++ sequences/cir_realign.py
@@ -1,11 +1,9 @@
-
-#set the working directory
 #open the .fasta file for raw alignment
 fh=open("raw_data.fasta",'r')
 
 sequences=[]  
 for line in fh.readlines():
-    #for a line starting fromm '>'
+    #for a line starting from '>'
     if line[0]=='>':
         name=line[1:].replace("\n",'')
         entry={'Species':name, 'sequence':''}
@@ -15,13 +13,13 @@ for line in fh.readlines():
 fh.close()
 
 #setting the cut-off for sequences, start position for the first common unit
-cutsite= 658
+cutsite= 662
 
 output=open("sequences.fasta", "w")
 for seq in sequences:
     seq['sequence'][:cutsite] #looking at the seq in 'seq' key and reading it from [:cutsite]
     seq['sequence']=seq['sequence'][cutsite:]+seq['sequence'][:cutsite] #rearranging the sequences
-    output.write('>' + seq['Species'] + '\n' + seq['sequence'] + '\n')
+    output.write('>' + seq['Species'] + '\n' + seq['sequence'] + '\n') #write output within the loop to go through the whole list of dictionaries
 print(sequences)
 
 output.close()
