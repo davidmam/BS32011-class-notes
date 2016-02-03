3c7b8936d4fb667abd7083707f9e2aa5b5b60e6b
Maxim <m.i.tsenkov@dundee.ac.uk>
2016-02-03 13:15:22 +0000

    updating script as pos=0 is for one list only

--- scripts/align_emboss.py
+++ scripts/align_emboss.py
@@ -28,17 +28,18 @@ species_b = ['TTAA', 'ASST', 'RGCY', 'AGCT', 'CNNR']
 output_a=open("species_a_rs.restrict", "w")
 output_b=open("species_b_rs.restrict", "w")
 
-pos=0 #setting a variable to the position of the restriction site
+pos_a=0 #setting a variable to the position of the restriction site in species_a
+pos_b=0 #setting a variable to the position of the restriction site in specoes_b
 
-for species_a[pos] in range(len[species_a]):
-    if species_a[pos]==species_b[pos]:
-        pos += 1
-    elif species_a[pos] < species_b[pos]:
-        outputa.write("\n".join(species_a[pos])) #adding unique restriction site based on current position
-    elif species_a[pos] > species_b[pos]:
-        while species_a[pos] > species_b[pos]:
-            output_b.write("\n".join(species_b[pos]))
-            pos += 1
+for species_a[pos_a] in range(len[species_a]):
+    if species_a[pos_a]==species_b[pos_b]:
+        pos_b += 1
+    elif species_a[pos_a] < species_b[pos_b]:
+        outputa.write("\n".join(species_a[pos_a])) #adding unique restriction site based on current position
+    elif species_a[pos_a] > species_b[pos_b]:
+        while species_a[pos_a] > species_b[pos_b]:
+            output_b.write("\n".join(species_b[pos_b]))
+            pos_b += 1
 
 output_a.close()
 output_b.close()
