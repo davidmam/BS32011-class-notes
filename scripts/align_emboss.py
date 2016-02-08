46511ae41e971daf16f07863ea2df9d4497ad656
David Martin <d.m.a.martin@dundee.ac.uk>
2016-02-08 12:09:30 +0000

    changes from lab meeting

--- scripts/align_emboss.py
+++ scripts/align_emboss.py
@@ -1,21 +1,50 @@
+import emboss_read
+seq_dir = '../sequences/individual/'
+species_a =  "e_europaeus"
+species_b =  "f_silvestris"
+sites_a = emboss_read.getsites(seq_dir +species_a + '.restrict', seq_dir +species_a + '.fasta')
+sites_b = emboss_read.getsites(seq_dir +species_b + '.restrict', seq_dir +species_b + '.fasta')
 output_a=open("species_a_rs.restrict", "w")
 output_b=open("species_b_rs.restrict", "w")
+jalview=open('features.txt','w')
+
+jalview.write('restrictionsite\tff00ff\n')
+
 
 #setting a variable to the position of the restriction site in species_a/species_b
 pos_a=0
 pos_b=0
 
+counter_a = 0
+counter_b = 0
+
+def formatsite(site):
+    return '|'.join([str(site['Start']), str(site['gappedstart']), 
+              site['Enzyme_name'], site['Restriction_site']])
+def jalview_out(site, species):
+    return '\t'.join([site['Enzyme_name'],species,'-1',
+               str(site['gappedstart']),
+            str(site['gappedstart']+len(site['Restriction_site'])),
+                'restrictionsite'])
 #loop
-while sites_a[pos_a]['gapstart'] < len(sites_a):
-    if sites_a[pos_a]['gapstart'] == sites_b[pos_b]['gapstart']:
+while sites_a[pos_a]['gappedstart'] < len(sites_a):
+    if sites_a[pos_a]['gappedstart'] == sites_b[pos_b]['gappedstart']:
         pos_a += 1
         pos_b += 1
-    elif sites_a[pos_a]['gapstart'] < sites_b[pos_b]['gapstart']:
-        output_a.write("\n".join(sites_a[pos_a]))
+    elif sites_a[pos_a]['gappedstart'] < sites_b[pos_b]['gappedstart']:
+        output_a.write(formatsite(sites_a[pos_a])+ "\n")
         pos_a += 1
-    elif sites_a[pos_a]['gapstart'] > sites_b[pos_b]['gapstart']:
-        output_b.write("\n".join(sites_b[pos_b]))
+        counter_a += 1
+        jalview.write(jalview_out(sites_a[pos_a], species_a)+'\n')
+    elif sites_a[pos_a]['gappedstart'] > sites_b[pos_b]['gappedstart']:
+        output_b.write(formatsite(sites_b[pos_b])+ "\n")
         pos_b += 1
+        counter_b += 1
+        jalview.write(jalview_out(sites_b[pos_b], species_b)+'\n')
+
+print(species_a + str(counter_a))
+print(species_b + str(counter_b))
 
 output_a.close()
 output_b.close()
+jalview.close()
--- scripts/emboss_read.py
+++ scripts/emboss_read.py
@@ -3,36 +3,38 @@
 # If the headers array is filled (ie if headers: ) then you can split the line into the data structure.
 # Start with identifying the lines you want to throw away and the lines you want to keep.
 
+def getsites(sitefile,seqfile):
 
-fh=open("e_europaeus.restrict",'r')
-headers=[]
-sites=[]
-
-#process the data
-for line in fh.readlines():
-	#read valid lines for lines not starting from '#'
-    if not line.startswith('#'):
-        if headers:
-			#strip and split values for valid headers
-            values=line.strip().split()
-            new_data=zip(headers,values)
-            new_dict=dict(new_data)
-            new_dict['sites'['Start']] = int(new_dict'sites'['Start'])
-			#append new dictionary to sites
-            sites.append(new_dict)
-        else:
-		#for line starting with space, find first valid header
-            if line.replace(' ','').startswith('Start'):
-                headers=line.strip().split()
-
-#calculate and define new start sites
-seq=fh.readlines("e_europaeus.fasta") #sequence with gaps read from dictionary
-gaps=0
-for line in fh.readlines():
-    len(seq[:'Start' + gaps]).replace('-','')
-for e in 'Restriction_site':
-    while len(seq[:'Start'] + gaps).replace('-',''):
-        gaps += len - e['Start']
+    fh=open(sitefile,'r')
+    headers=[]
+    sites=[]
+    
+    #process the data
+    for line in fh.readlines():
+    	#read valid lines for lines not starting from '#'
+        if not line.startswith('#'):
+            if headers:
+    			#strip and split values for valid headers
+                values=line.strip().split()
+                if values:
+                    new_data=zip(headers,values)
+                    new_dict=dict(new_data)
+                    for h in ['Start','End','3prime','5prime']:
+                        new_dict[h]=int(new_dict[h])
+    			#append new dictionary to sites
+                    sites.append(new_dict)
+            else:
+    		#for line starting with space, find first valid header
+                if line.replace(' ','').startswith('Start'):
+                    headers=line.strip().split()
+    
+    #calculate and define new start sites
+    fh=open(seqfile,'r')
+    seq=''.join(fh.readlines()[1:]).replace('\n','') #sequence with gaps read from dictionary
+    gaps=0
+    for e in sites:
+        while len(seq[:e['Start'] + gaps].replace('-','')) < e['Start']:
+            gaps +=e['Start'] - len(seq[:e['Start'] + gaps].replace('-','')) 
         e['gappedstart']=e['Start']+gaps 
-
-fh.close()
+    fh.close()
+    return sites
