f5bf2715c1dd306afb94bf8d78efe01ecbab007d
Maxim <m.i.tsenkov@dundee.ac.uk>
2016-02-14 15:26:23 +0000

    Created new script to further filter unique sites

---
+++ scripts/f_conserved.py
@@ -0,0 +1,12 @@
+import align_emboss
+
+jalview_f=open('features_f.txt','w')
+jalview_f.write('restrictionsite\t00FFF3\n')
+
+renzyme_list = ['SgeI', 'FaiI', 'MspJI', 'SetI', 'Tsp45I', 
+                'EcorII', 'TfiI', 'HindII', 'SfeI', 'SphI', 
+                'LpnPI', 'BmgT120I', 'DrdI', 'NspI']
+
+for rang() in output_a:
+    if sites_a in output_a:
+        pass
