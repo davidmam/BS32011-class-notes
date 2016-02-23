2221d16eca4e547040c89f22ca37248e2b3b386d
Imogen Stafford <i.s.stafford@dundee.ac.uk>
2016-02-23 13:33:38 +0000

    adding primer3 files

---
+++ ImogenS/primer3_m_erminea.py
@@ -0,0 +1,20 @@
+primer3params={'Sequence_ID': 'm_erminea', 
+               'Sequence_Included_Region': '4438,13338',
+               'SEQUENCE_TARGET': '8000,50',
+               'PRIMER_PRODUCT_SIZE_RANGE':'400-800',
+               'PRIMER_MAX_TM': '61',
+               'PRIMER_MIN_TM': '59'}
+
+# get the sequence
+sequencefile='../ImogenS/m_erminea.fasta'
+
+primer3params['SEQUENCE']=''.join(open(sequencefile).readlines()[1:]).replace('\n','')
+
+#sequence_target is start site +/- a space around it. 50bp?
+
+p3i=open('primer3input.txt','w')
+
+for k in primer3params.keys():
+    p3i.write('%s=%s'%(k,primer3params[k] + '\n'))
+
+p3i.close()
