45e4eb0f376a67343bc51d69949e0bf251131b59
Simon Bajew <s.p.bajew@dundee.ac.uk>
2016-02-23 17:01:29 +0000

    moderated scripts for Primer3

---
+++ scripts/primer3_lupus.py
@@ -0,0 +1,22 @@
+#moderated by Simon Bajew on 23/02/2016
+
+#setting up parameters for Primer3
+primer3params={'Sequence_ID': 'l_europaeus', 
+               'Sequence_Included_Region': '4438,13338',
+               'SEQUENCE_TARGET': '5000,50',
+               'PRIMER_PRODUCT_SIZE_RANGE':'400-800',
+               'PRIMER_MAX_TM': '61',
+               'PRIMER_MIN_TM': '59'}
+
+#getting the sequence in FASTA file
+sequencefile='../sequences/individual/l_europaeus.fasta'
+
+#replacing gaps in the sequence
+primer3params['SEQUENCE']=''.join(open(sequencefile).readlines()[1:]).replace('\n','').replace('-','')
+
+#writing Primer3 output file
+p3i=open('primer3_lupus.txt','w')
+for k in primer3params.keys():
+    p3i.write('%s=%s'%(k,primer3params[k] + '\n'))
+
+p3i.close()
